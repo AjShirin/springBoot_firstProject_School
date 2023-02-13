31acb2c987da4b18be083b6801314fe693813a69
AjShirin <shirinajmi@gmail.com>
2023-02-13 11:27:57 +0400

    testing something

--- APIProjectFirst_Shirin/src/main/java/com/codeline/API/APIProjectFirst_Shirin/Repositories/StudentRepository.java
+++ APIProjectFirst_Shirin/src/main/java/com/codeline/API/APIProjectFirst_Shirin/Repositories/StudentRepository.java
@@ -1,7 +1,5 @@
 package com.codeline.API.APIProjectFirst_Shirin.Repositories;
 
-import com.codeline.API.APIProjectFirst_Shirin.Models.Mark;
-import com.codeline.API.APIProjectFirst_Shirin.Models.School;
 import com.codeline.API.APIProjectFirst_Shirin.Models.Student;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -26,8 +24,8 @@ public interface StudentRepository extends CrudRepository<Student, Integer> {
     Student getStudentById(@Param("studentId") Integer id);  // whoever called the function and write the ID, it will be mapped with the ID of the school
     // calling the function from the service
 
+    // the 'name' should be the same as the java
     @Query("SELECT s from Student s where s.name= :name")
-        // the 'name' should be the same as the java
-    Student getStudentByName(@Param("name") String student_name); // mapping the query and returning the student
-
+    // mapping the query and returning the student
+    Student getStudentByName(@Param("name") String student_name);
 // To do in the postman localhost:8080/student/getByStudentName?student_name(sql column)=shirin
